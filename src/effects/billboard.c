dda86a8a3eba949b595ec5b80c2ee6000aa5fb2e
Alex Hiatt <alexryanhiatt@gmail.com>
2023-01-19 11:42:01 -0600

    continued work on billboards

--- src/effects/billboard.c
+++ src/effects/billboard.c
@@ -14,27 +14,35 @@ void billboard__draw(
   static struct vec3 forward, right, up; 
   static struct m4x4 lookat, translation, local_to_world;
 
-  forward = vec3__normalize(vec3_minus_vec3(
-    cam->position,
-    bb->transform.position
-  ));
-  right = vec3__normalize(vec3__cross(
-    WORLDSPACE.up,
-    forward
-  ));
-  up = vec3__normalize(vec3__cross(
-    forward, right
-  ));
+  bb->transform.rotation_in_deg.y += 0.05f;
 
-  m4x4__view(right, up, forward, &lookat);
-  m4x4__translation(bb->transform.position, &translation);
-  // m4x4__scaling(t->scale, &scale);
-
-  m4x4_x_m4x4(
-    &lookat,
-    &translation,
+  space__create_model(
+    &WORLDSPACE,
+    &bb->transform,
     &local_to_world
   );
+
+  // forward = vec3__normalize(vec3_minus_vec3(
+  //   cam->position,
+  //   bb->transform.position
+  // ));
+  // right = vec3__normalize(vec3__cross(
+  //   WORLDSPACE.up,
+  //   forward
+  // ));
+  // up = vec3__normalize(vec3__cross(
+  //   forward, right
+  // ));
+
+  // m4x4__view(right, up, forward, &lookat);
+  // m4x4__translation(bb->transform.position, &translation);
+  // // m4x4__scaling(t->scale, &scale);
+
+  // m4x4_x_m4x4(
+  //   &lookat,
+  //   &translation,
+  //   &local_to_world
+  // );
 
   gpu->select_shader(bb->shader);
   gpu->select_texture(bb->texture);
